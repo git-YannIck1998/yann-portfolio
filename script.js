a85e597e1c4643a6097322d04bbdc017fa98ca4a
gityanno1998 <yannagre1997@gmail.com>
2023-12-01 21:23:37 +0300

    Format change

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
@@ -20,30 +20,31 @@ for (i = 0; i < coll.length; i++) {
 
 // Button: Get Started with me
 function getStarted() {
-    const content = document.getElementById("content")
-    content.innerHTML='<object type="application/pdf" data="/extras/ayecv.pdf" width="100%" height="1000px" no-download></object>';
-    window.addEventListener('contextmenu', function (e) {
-      e.preventDefault(); // Prevent right-click context menu
-    });
+  const content = document.getElementById("content");
+  content.innerHTML =
+    '<object type="application/pdf" data="/extras/ayecv.pdf" width="100%" height="1000px" no-download></object>';
+  window.addEventListener("contextmenu", function (e) {
+    e.preventDefault(); // Prevent right-click context menu
+  });
 
-    document.getElementById('viewPDF').addEventListener('click', function () {
-      alert('Sorry, downloading PDF is not allowed.'); // Display custom message
-    });
+  document.getElementById("viewPDF").addEventListener("click", function () {
+    alert("Sorry, downloading PDF is not allowed."); // Display custom message
+  });
 }
 
 //Socials Redirect
 function facebook() {
-  window.open( "https://www.facebook.com", "_blank");
+  window.open("https://www.facebook.com", "_blank");
 }
 
 function github() {
-  window.open( "", "_blank");
+  window.open("", "_blank");
 }
 
 function twitter() {
-  window.open( "https://www.twitter.com", "_blank");
+  window.open("https://www.twitter.com", "_blank");
 }
 
 function instagram() {
-  window.open( "https://www.instagram.com", "_blank");
+  window.open("https://www.instagram.com", "_blank");
 }
